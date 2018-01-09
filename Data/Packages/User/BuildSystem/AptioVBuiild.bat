c8c07b6045c7f88687f8822d230d0d4e80716ca8
liuqp1 <liuqp1@lenovo.com>
2018-01-09 11:12:08 +0800

    20180109

--- Data/Packages/User/BuildSystem/AptioVBuiild.bat
+++ Data/Packages/User/BuildSystem/AptioVBuiild.bat
@@ -1,10 +1,10 @@
 @echo on
 
-set PATH=D:\CommonTools\WinDDK\7600.16385.1\bin\x86;D:\CommonTools\Aptio_5.x_30\BuildTools;%PATH%
+set PATH=E:\BuildTool\WinDDK\7600.16385.1\bin\x86;E:\BuildTool\Aptio_5.x_30\BuildTools;%PATH%
 
-set CCX86DIR=D:\CommonTools\WinDDK\7600.16385.1\bin\x86\x86
-set CCX64DIR=D:\CommonTools\WinDDK\7600.16385.1\bin\x86\amd64
-set TOOLS_DIR=D:\CommonTools\Aptio_5.x_30\BuildTools
+set CCX86DIR=E:\BuildTool\WinDDK\7600.16385.1\bin\x86\x86
+set CCX64DIR=E:\BuildTool\WinDDK\7600.16385.1\bin\x86\amd64
+set TOOLS_DIR=E:\BuildTool\Aptio_5.x_30\BuildTools
 if exist PurleyLenovo.veb (
 	set VEB=PurleyLenovo
 ) else (
